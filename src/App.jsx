8658bb4b0f6e79ee3242527ccddc0d24d966abad
ImanolCB <imacallebara@gmail.com>
2024-09-27 20:15:47 +0200

    Check winner

--- src/App.jsx
+++ src/App.jsx
@@ -23,17 +23,46 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   )
 }
 
-
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 function App() {
 
   const [board , setBoard] = useState(Array(9).fill(null))
   const [turn , setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) //Null -> no hay ganador, False es empate
+
+  const checkWinner = (boardTocheck) => {
+
+    //Revisión de combinaciones ganadoras
+    for(const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardTocheck[a] &&
+        boardTocheck[a] === boardTocheck[b] &&
+        boardTocheck[a] === boardTocheck[c]
+
+      ) {
+        return boardTocheck[a]
+      }
+    }
+    //Si no hay ganador
+    return null
+  }
+
 
   const updateBoard = (index) =>{
 
     //No se modifica la celda si hay algo en esa posición
-    if(board[index]) return
+    if(board[index] || winner) return
 
     //Copia el tablero y se actualiza con los nuevos datos
     const newBoard = [...board]
@@ -43,6 +72,13 @@ function App() {
     //Cambia el estado de turno
     const newTurn = turn ===TURNS.X ? TURNS.O :TURNS.X
     setTurn(newTurn)
+
+    //Revisión si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      alert('Ganador')
+      setWinner(newWinner)
+    }
   }
 
   return (
